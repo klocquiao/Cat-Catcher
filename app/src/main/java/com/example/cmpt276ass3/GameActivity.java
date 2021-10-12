7dc13079f9cfc0fadd344d80534b46d84f2ec058
klocq <knl5@sfu.ca>
2021-10-11 23:47:46 -0700

    Implement basic layout for main menu
    Main menu has 3 of its core buttons as well as a textview that shows that the current activity is activity_main_menu.java. Start button is able to start a game, however configuration is absent and must be implemented.

--- app/src/main/java/com/example/cmpt276ass3/GameActivity.java
+++ app/src/main/java/com/example/cmpt276ass3/GameActivity.java
@@ -2,6 +2,10 @@ package com.example.cmpt276ass3;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +24,7 @@ public class GameActivity extends AppCompatActivity {
 
     private static final int NUM_ROWS = 3;
     private static final int NUM_COLS = 3;
+    Button cellArray[][] = new Button[NUM_ROWS][NUM_COLS];
     private AppBarConfiguration appBarConfiguration;
     private ActivityGameBinding binding;
 
@@ -45,32 +50,64 @@ public class GameActivity extends AppCompatActivity {
                 1.0f));
             table.addView(tableRow);
             for (int col = 0; col < NUM_COLS; col++) {
+                final int FINAL_ROW = row;
+                final int FINAL_COL = col;
                 Button button = new Button(this);
 
+                //Format
                 button.setLayoutParams(new TableRow.LayoutParams(
                 TableRow.LayoutParams.MATCH_PARENT,
                 TableRow.LayoutParams.MATCH_PARENT,
                 1.0f));
-
-                button.setText("");
                 button.setPadding(0, 0, 0, 0);
 
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        cellButtonClicked(button);
+                        cellButtonClicked(FINAL_ROW, FINAL_COL);
                     }
                 });
 
                 tableRow.addView(button);
+                cellArray[row][col] = button;
             }
 
         }
     }
 
-    private void cellButtonClicked(Button bt) {
+    private void lockButtonSize() {
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                Button button = cellArray[row][col];
+                int width = button.getWidth();
+                button.setMinWidth(width);
+                button.setMaxWidth(width);
+
+                int height = button.getHeight();
+                button.setMinHeight(height);
+                button.setMaxHeight(height);
+            }
+        }
+    }
+
+    private void cellButtonClicked(int row, int col) {
         Toast.makeText(this, "TEMP", Toast.LENGTH_LONG).show();
-        bt.setText("X");
+        Button button = cellArray[row][col];
+
+        //Lock button sizes
+        lockButtonSize();
+
+        //Scale drawable
+        scaleImageToCell(button);
+    }
+
+    private void scaleImageToCell(Button button) {
+        int newWidth = button.getWidth();
+        int newHeight = button.getHeight();
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_android_black);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+        Resources resource = getResources();
+        button.setBackground(new BitmapDrawable(resource, scaledBitmap));
     }
 
     public static Intent newIntent(Context c) {
--- app/src/main/java/com/example/cmpt276ass3/WelcomeActivity.java
+++ app/src/main/java/com/example/cmpt276ass3/WelcomeActivity.java
@@ -4,16 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.cmpt276ass3.databinding.ActivityWelcomeBinding;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,36 +27,14 @@ public class WelcomeActivity extends AppCompatActivity {
         binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
 
         startGame();
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void startGame() {
-        Button startButton = (Button) findViewById(R.id.startGameButton);
+        Button startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
